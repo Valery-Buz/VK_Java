211008782571cae72be800f4a43827bcafdc2fb1
Valery Buz <Ajven@yandex.ru>
2022-03-09 22:58:54 +0300

    v1

---
+++ src/main/java/domain/PostVK.java
@@ -0,0 +1,21 @@
+package domain;
+
+import org.w3c.dom.Text;
+
+public class PostVK {
+
+    private int id;
+    private String date;
+    private int time;
+    private String namePost;
+    private String imageUrl;
+    private String textUrl;
+    private Likes likes;
+    private CommentsInfo commentsInfo;
+    private RepostsPost repostsPost;
+    private ViewsInfo viewsInfo;
+
+
+
+
+}
